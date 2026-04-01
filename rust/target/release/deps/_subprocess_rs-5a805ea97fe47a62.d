53868f115c2b7a5c1ef175941a1ffd724494c6f7
Bob the Builder <bob@hermes>
2026-04-01 01:58:23 +0000

    rust_optimizer: auto-apply 7305 optimizations

---
+++ rust/target/release/deps/_subprocess_rs-5a805ea97fe47a62.d
@@ -0,0 +1,5 @@
+/root/.hermes/hermes-agent-ferris-fork/rust/target/release/deps/_subprocess_rs-5a805ea97fe47a62.d: subprocess_rs/src/lib.rs
+
+/root/.hermes/hermes-agent-ferris-fork/rust/target/release/deps/lib_subprocess_rs-5a805ea97fe47a62.rmeta: subprocess_rs/src/lib.rs
+
+subprocess_rs/src/lib.rs:
---
+++ rust/target/release/deps/url_safety_rs-9882af53e7e01b52.d
@@ -0,0 +1,5 @@
+/root/.hermes/hermes-agent-ferris-fork/rust/target/release/deps/url_safety_rs-9882af53e7e01b52.d: url_safety_rs/src/lib.rs
+
+/root/.hermes/hermes-agent-ferris-fork/rust/target/release/deps/liburl_safety_rs-9882af53e7e01b52.rmeta: url_safety_rs/src/lib.rs
+
+url_safety_rs/src/lib.rs:
